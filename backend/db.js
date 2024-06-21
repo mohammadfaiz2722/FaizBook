03308266f1c7b920763325dd9f1a271731d66106
mohammadfaiz2722 <133015621+mohammadfaiz2722@users.noreply.github.com>
2024-06-21 23:51:11 +0530

    updated

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,16 @@
-
-
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://127.0.0.1:27017/task2"; // Replace with your MongoDB connection string
-let connectToMongo=()=>
-{
-    
-    mongoose.connect( mongoURI, {useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB:', error);
-    });
+// console.log('Loaded MONGO_URI:', process.env.REACT_APP_API_URL); // Add this line for debugging
 
-   
-}
+const connectToMongo = () => {
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log('Connected to MongoDB');
+        })
+        .catch((error) => {
+            console.error('Error connecting to MongoDB:', error);
+        });
+};
 
 module.exports = connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const connectToMongo = require('./db');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
