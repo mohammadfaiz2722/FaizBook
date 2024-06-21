9b4cc7807dfc452c76b45b46b2456f60d18249ca
mohammadfaiz2722 <133015621+mohammadfaiz2722@users.noreply.github.com>
2024-06-22 00:11:09 +0530

    Updated

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 // console.log('Loaded MONGO_URI:', process.env.REACT_APP_API_URL); // Add this line for debugging
+const mongoUri=process.env.MONGO_URI || "mongodb://127.0.0.1:27017/task2"
 
 const connectToMongo = () => {
-    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log('Connected to MongoDB');
         })
--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const connectToMongo = require('./db');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+const path=require('path')
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,11 +13,14 @@ connectToMongo();
 // Middleware
 app.use(cors()); // Enable CORS
 app.use(bodyParser.json()); // Parse JSON request bodies
+app.use(express.static(path.join(__dirname,'./build')))
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/posts', require('./routes/posts'));
-
+app.use('*',function(req,res){
+  res.sendFile(path.join(__dirname,'./build/index.html'))
+})
 // Start the server
 app.listen(port, () => {
   console.log(`Backend server is running at http://localhost:${port}`);
